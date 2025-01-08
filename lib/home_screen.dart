ccb63f8ddcbea4c1267db565dfdaa0a6f0bb7e16
githcross <63359695+githcross@users.noreply.github.com>
2025-01-08 13:48:45 -0600

    Update home_screen.dart
    se implementó logout al home_screen está funcional.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'auth_service.dart';
+import 'login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  final AuthService _authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Pantalla Principal"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await _authService.signOut();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginScreen()),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text("Bienvenido a la aplicación!"),
